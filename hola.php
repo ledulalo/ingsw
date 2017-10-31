63eb65b82a3aae851de010cf341aec4a22d0e518
ledulalo <obi_22@hotmail.com>
2017-10-31 14:42:54 -0600

    esta es una prueba

---
+++ hola.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="apple-touch-icon" sizes="57x57" href="img/icono/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="img/icono/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="img/icono/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="img/icono/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="img/icono/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="img/icono/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="img/icono/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="img/icono/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="img/icono/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192"  href="img/icono/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="img/icono/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="img/icono/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="img/icono/favicon-16x16.png">
+    <link rel="manifest" href="img/icono/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+    <link rel="stylesheet" type="text/css" href="css/nav.css">
+    <link rel="stylesheet" type="text/css" href="css/footer.css">
+    <link href="https://fonts.googleapis.com/css?family=Merriweather" rel="stylesheet"> 
+    <meta name="theme-color" content="#ffffff">
+	<title>Goga Manualidades</title>
+</head>
+<body>
+<header>
+    <a href="#" id="logo"><img src="img/logo.png" class="imagenlogo"></a>
+	<nav>
+     <a href="#" id="menu"></a>
+     <ul>
+         <li><a href="#">Inicio</a></li>
+         <li><a href="#">Contactanos</a></li>
+         <li><a href="#">Catalogo</a></li>
+         <li><a href="#">Preguntas Frecuentes</a></li>
+     </ul>   
+    </nav>
+</header>
+<section>
+
+</section>
+<footer>
+ <div id="footer">
+  
+   <ul class="social">
+       <li> <p class="copyright_text" >&copy; copyright 2017 Hilda Martin del campo</br> designed and developed by Eduardo Echeverria</p> </li>
+       <li><a href="https://www.facebook.com/hilda.martindelcampo" target="_blank"><img src="img/fb.png"></a></li>
+       <li><a href="#"><img src="img/instagram.png"></a></li>
+   </ul>
+ </div>
+</footer>
+</body>
+</html>
